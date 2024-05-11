32151823de231b038cd9988614a0836fd28be109
asiazhang <asiazhang2002@gmail.com>
2024-05-11 17:06:31 +0800

    增加二进制文件入口

--- src/solar_registry/cli.py
+++ src/solar_registry/cli.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 import typer
 
+from typing import Optional
+from typing_extensions import Annotated
+
 from .commands.meta_merger import MetaMerger
 from .service.pr_generator import PullRequestGenerator
 from .service.testtool import get_testtool
@@ -10,7 +13,7 @@ app = typer.Typer()
 
 
 @app.command()
-def merge(tool_name: str, output: str, working_dir: str | None = None) -> None:
+def merge(tool_name: str, output: str, working_dir: Optional[str] = None) -> None:
     """
     合并工具版本元数据
 
@@ -24,15 +27,20 @@ def merge(tool_name: str, output: str, working_dir: str | None = None) -> None:
 
 
 @app.command()
-def merge_and_pull_request(tool_name: str, working_dir: str | None = None) -> None:
+def pull_request(
+    tool_name: Annotated[str, typer.Argument(help="工具名称")],
+    working_dir: Annotated[Optional[str], typer.Argument(help="可选工作目录")] = None,
+) -> None:
     """
     合并元数据之后，向项目提PR进行合并操作
-    :param tool_name: 工具名称
-    :param working_dir: 可选工作目录
     """
     testtool = get_testtool(tool_name, working_dir)
     pr_gen = PullRequestGenerator(testtool)
     pr_gen.merge_and_create_pull_request()
+
+
+def cli_entry() -> None:
+    app()
 
 
 if __name__ == "__main__":
