14207faed6f5c42b2021df05670ee2dc755680eb
asiazhang <asiazhang2002@gmail.com>
2024-05-22 19:18:14 +0800

    增加gitPkgUrl属性，用于指定Git模式地址

--- tests/test_testtool.py
+++ tests/test_testtool.py
@@ -14,6 +14,7 @@ def test_validate_correct_pytest_tool() -> None:
 
     assert tool.name == "pytest"
     assert tool.version == "0.1.6"
+    assert tool.git_pkg_url == "github.com/OpenTestSolar/testtool-python@main:pytest"
 
     assert tool.support_os[0] == OsType.Windows
     assert tool.support_os[1] == OsType.Linux
