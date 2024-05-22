fb7da528bba3e6ee128b9c1fca31645431f41035
asiazhang <asiazhang2002@gmail.com>
2024-05-22 19:40:45 +0800

    优化错误信息

--- src/solar_registry/model/test_tool.py
+++ src/solar_registry/model/test_tool.py
@@ -74,9 +74,9 @@ class TestTool(BaseModel):
         直接在模型中增加非None检查会导致旧版本的测试工具元数据解析报错，所以单独提取一个函数用于校验，需要的时候再调用
         """
 
-        assert self.support_os, "should have support_os in yaml"
-        assert self.support_arch, "should have support_arch in yaml"
-        assert self.git_pkg_url, "should have git_pkg_url in yaml"
+        assert self.support_os, "should have supportOS in yaml"
+        assert self.support_arch, "should have supportArch in yaml"
+        assert self.git_pkg_url, "should have gitPkgUrl in yaml"
 
 
 class TestToolTarget(BaseModel):
