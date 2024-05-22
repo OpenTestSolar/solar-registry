3284aea845fb4dee6a38b471af94d036b4d9f552
asiazhang <asiazhang2002@gmail.com>
2024-05-22 10:46:50 +0800

    COS支持自定义endpoint，并支持上传测试工具压缩包到COS

--- src/solar_registry/service/cos_sync.py
+++ src/solar_registry/service/cos_sync.py
@@ -1,21 +1,30 @@
 import hashlib
 import os
+import tarfile
 import tempfile
+from pathlib import Path
+from typing import Callable
 
 from loguru import logger
-
-from pathlib import Path
-
 from qcloud_cos import CosS3Client, CosConfig  # type: ignore[import-untyped]
+
+from ..model.asset import gen_asset_relative_path
+from ..model.test_tool import TestTool
 
 
 class CosSyncService:
-    def __init__(self, workdir: str | None = None) -> None:
+    def __init__(
+        self,
+        workdir: str | None = None,
+        endpoint_gen: Callable[[str], str] | None = None,
+    ) -> None:
         self.workdir = workdir or os.getcwd()
         if "COS_SECRET_ID" not in os.environ:
             raise ValueError("Environment COS_SECRET_ID variables not set")
+        self.cos_secret_id = os.environ["COS_SECRET_ID"]
         if "COS_SECRET_KEY" not in os.environ:
             raise ValueError("Environment COS_SECRET_KEY variables not set")
+        self.cos_secret_key = os.environ["COS_SECRET_KEY"]
         if "COS_REGION" not in os.environ:
             raise ValueError("Environment COS_REGION variables not set")
         self.cos_region = os.environ["COS_REGION"]
@@ -23,13 +32,21 @@ class CosSyncService:
             raise ValueError("Environment COS_BUCKET variables not set")
         self.cos_bucket = os.environ["COS_BUCKET"]
 
-        self.cos_client = CosS3Client(
-            CosConfig(
-                Region=os.environ["COS_REGION"],
-                SecretId=os.environ["COS_SECRET_ID"],
-                SecretKey=os.environ["COS_SECRET_KEY"],
+        if endpoint_gen:
+            config = CosConfig(
+                Region=self.cos_region,
+                SecretId=self.cos_secret_id,
+                SecretKey=self.cos_secret_key,
+                Endpoint=endpoint_gen(self.cos_region),
             )
-        )
+        else:
+            config = CosConfig(
+                Region=self.cos_region,
+                SecretId=self.cos_secret_id,
+                SecretKey=self.cos_secret_key,
+            )
+
+        self.cos_client = CosS3Client(config)
 
     def sync_meta_data(self, force: bool) -> None:
         for dir_path, _, filenames in os.walk(Path(self.workdir) / "testtools"):
@@ -80,13 +97,26 @@ class CosSyncService:
                     relative_file=relative_file, full_path=str(full_path)
                 )
 
+    def upload_archive_file(self, test_tool: TestTool) -> None:
+        # 生成对应测试工具的压缩包，并上传到COS的指定位置
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_file = Path(tmpdir) / f"{test_tool.name}.tar.gz"
+            with tarfile.open(output_file, "w:gz") as tar:
+                tar.add(Path(self.workdir) / test_tool.name, arcname=test_tool.name)
+
+            cos_file_path = gen_asset_relative_path(testtool=test_tool)
+            logger.info(f"Uploading {cos_file_path} to COS bucket {self.cos_bucket}...")
+            self.upload_file_to_cos(
+                relative_file=cos_file_path, full_path=str(output_file)
+            )
+            logger.info(f"✅ Uploaded {cos_file_path} to COS bucket {self.cos_bucket}")
+
     def upload_file_to_cos(self, relative_file: str, full_path: str) -> None:
         response = self.cos_client.upload_file(
             Bucket=self.cos_bucket,
             Key=relative_file,
             LocalFilePath=full_path,
             EnableMD5=True,
-            ACL="public-read",
             progress_callback=None,
         )
         logger.info(
