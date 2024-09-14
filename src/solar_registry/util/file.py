4c42440e9663c9a1f4b678d66a3057b7f095a717
asiazhang <asiazhang2002@gmail.com>
2024-09-14 15:17:34 +0800

    Replace retry library with opnieuw (#44)
    * 替换retry库和requests库，使用opnieuw和httpx
    * 当前设置为Stable版本

--- src/solar_registry/util/file.py
+++ src/solar_registry/util/file.py
@@ -1,20 +1,17 @@
 from pathlib import Path
 from typing import Union
-import requests
+
+import httpx
 from loguru import logger
-from retry import retry
+from opnieuw import retry
 
 
-@retry(tries=5, delay=1, backoff=2)
+@retry(retry_on_exceptions=httpx.HTTPError)
 def download_file_to(url: str, to_file: Union[str, Path]) -> None:
     logger.info(f"Downloading {url} to {to_file}")
-    response = requests.get(url, stream=True)
+
+    response = httpx.get(url, follow_redirects=True)
     response.raise_for_status()
 
     with open(to_file, "wb") as f:
-        # 逐块写入文件内容
-        for chunk in response.iter_content(chunk_size=65536):
-            if chunk:  # 过滤掉保持连接活动的新块
-                f.write(chunk)
-
-        logger.info(f"Downloaded {url} to {to_file}")
+        f.write(response.content)
